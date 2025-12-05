7e79f8919fba1fba23bb181e3543efc4315790b3
YMB06 <youssefmf240@gmail.com>
2025-12-05 11:38:13 +0100

    boton con menu de hamburguesa creado con su script

---
+++ web-res/js/script.js
@@ -0,0 +1,6 @@
+//funcion para abrir y cerrar el menu de hamburguesa para dispositivos moviles
+function toggleMenu() {
+    const navMenu = document.getElementById('navMenu');
+    navMenu.classList.toggle('active');
+}
+
